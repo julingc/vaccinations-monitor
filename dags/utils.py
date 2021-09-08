e5c0af77cd7032e0a3c0acc2f08cf8c57f832e77
julingc <julingjchang@gmail.com>
2021-09-08 13:28:09 +0800

    submit airflow

---
+++ dags/utils.py
@@ -0,0 +1,76 @@
+import os
+import pandas as pd
+from google.cloud import bigquery
+from airflow.providers.google.cloud.hooks.bigquery import BigQueryHook
+
+
+# Config variables
+BQ_CONN_ID = 'bigquery_connection'
+BQ_PROJECT = 'vaccination-monitor'
+BQ_DATASET = 'vaccinations'
+table_id = 'vaccination-monitor.vaccinations.daily-vaccinations'
+
+
+def get_vaccination_data(url: str, filename:str):
+    df = pd.read_csv(url)
+    df = df[['location', 'date', 'daily_vaccinations_per_million']]
+    df['date'] = pd.to_datetime(df['date'])
+
+    # Extract necessary data and output a csv for loading to BigQuery
+    df.to_csv(filename, index=False)
+    csv_rows = len(df.index)
+    return csv_rows
+
+
+def check_bq_rows():
+    hook = BigQueryHook(bigquery_conn_id=BQ_CONN_ID, use_legacy_sql=False)
+    client = bigquery.Client(project=hook._get_field(
+        "project"), credentials=hook._get_credentials())
+    table = client.get_table(table_id)
+    bq_rows = table.num_rows
+    return bq_rows
+
+
+def compare(**context):
+    csv_rows = context['task_instance'].xcom_pull(
+        task_ids='extract_vaccination_data')
+    bq_rows = context['task_instance'].xcom_pull(task_ids='get_bq_rows')
+
+    if csv_rows == bq_rows:
+        return 'remove_csv'
+    else:
+        return 'vaccination_data_to_bq'
+
+
+def load_data_to_bq(filename: str, remove_local=False):
+    vaccinations_df = pd.read_csv(filename)
+    vaccinations_df['date'] = pd.to_datetime(vaccinations_df['date'])
+
+    hook = BigQueryHook(bigquery_conn_id=BQ_CONN_ID, use_legacy_sql=False)
+    client = bigquery.Client(project=hook._get_field(
+        "project"), credentials=hook._get_credentials())
+
+    # Overwrite table data with the lastest vaccinations.csv
+    job_config = bigquery.job.LoadJobConfig(
+        schema=[
+            bigquery.SchemaField(
+                'location', bigquery.enums.SqlTypeNames.STRING),
+            bigquery.SchemaField('date', bigquery.enums.SqlTypeNames.DATE),
+            bigquery.SchemaField(
+                'daily_vaccinations_per_million', bigquery.enums.SqlTypeNames.INTEGER)
+        ],
+        write_disposition='WRITE_TRUNCATE',
+    )
+    load_job = client.load_table_from_dataframe(
+        vaccinations_df, table_id, job_config=job_config)
+    load_job.result()
+
+    # Remove local csv file once data loaded to bigquery
+    if remove_local:
+        if os.path.isfile(filename):
+            os.remove(filename)
+
+
+def remove_local(filename: str):
+    if os.path.isfile(filename):
+        os.remove(filename)
---
+++ dags/vaccinations_data.py
@@ -0,0 +1,76 @@
+from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow.operators.python import PythonOperator, BranchPythonOperator
+from airflow.operators.dummy import DummyOperator
+from utils import get_vaccination_data, compare, check_bq_rows, load_data_to_bq, remove_local
+
+
+# DAG definition
+default_args = {
+    'owner': 'Juling',
+    'email': ['julingjchang@gmail.com'],
+    'start_date': datetime(2021, 8, 28),
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 2,
+    'retry_delay': timedelta(minutes=1),
+}
+
+dag = DAG(
+    'etl_workflow',
+    default_args=default_args,
+    schedule_interval='@daily'
+)
+
+# Config variables
+BQ_CONN_ID = 'bigquery_connection'
+BQ_PROJECT = 'vaccination-monitor'
+BQ_DATASET = 'vaccinations'
+
+extract_vaccination_data = PythonOperator(
+    dag=dag,
+    task_id='extract_vaccination_data',
+    python_callable=get_vaccination_data,
+    op_kwargs={
+        'url': 'https://github.com/owid/covid-19-data/raw/master/public/data/vaccinations/vaccinations.csv?raw=true',
+        'filename': 'temp/vaccinations.csv',
+    },
+)
+
+get_bq_rows = PythonOperator(
+    dag=dag,
+    task_id='get_bq_rows',
+    python_callable=check_bq_rows,
+)
+
+compare_num_rows = BranchPythonOperator(
+    dag=dag,
+    task_id='compare_num_rows',
+    python_callable=compare,
+)
+
+vaccination_data_to_bq = PythonOperator(
+    dag=dag,
+    task_id='vaccination_data_to_bq',
+    python_callable=load_data_to_bq,
+    op_kwargs={
+        'filename': 'temp/vaccinations.csv',
+        'remove_local': 'true',
+    },
+)
+
+remove_csv = PythonOperator(
+    dag=dag,
+    task_id='remove_csv',
+    python_callable=remove_local,
+    op_kwargs={
+        'filename': 'temp/vaccinations.csv'
+    },
+
+)
+end_of_data_pipeline = DummyOperator(task_id='end_of_data_pipeline', dag=dag)
+
+extract_vaccination_data >> get_bq_rows >> compare_num_rows
+compare_num_rows >> vaccination_data_to_bq >> end_of_data_pipeline
+compare_num_rows >> remove_csv
